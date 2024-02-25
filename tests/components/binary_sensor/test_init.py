46fd681ffc899be3e711218581cdebcab7c82649
Robert Lippmann <rlippmann@hotmail.com>
2024-02-25 07:34:57 -0500

    add more test cases

--- tests/components/binary_sensor/test_init.py
+++ tests/components/binary_sensor/test_init.py
@@ -131,7 +131,100 @@ async def test_name(hass: HomeAssistant) -> None:
     assert state.attributes == {"device_class": "battery", "friendly_name": "Battery"}
 
 
-@pytest.mark.parametrize("device_class", list(binary_sensor.BinarySensorDeviceClass))
+sensor3_icon = "mdi:binary_sensor3"
+sensor4_icon = "mdi:binary_sensor4"
+sensor5_icon = "mdi:binary_sensor5"
+sensor6_icon = "mdi:binary_sensor6"
+
+
+class BinarySensorNoDefaultIcon(binary_sensor.BinarySensorEntity):
+    """Test binary sensor with no icon."""
+
+    def __init__(
+        self, binary_sensor_device_class: binary_sensor.BinarySensorDeviceClass
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__()
+        self._is_on: bool | None = None
+        self.device_class = binary_sensor_device_class
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return true if the binary sensor is on."""
+        return self._is_on
+
+    def turn_on(self) -> None:
+        """Turn the entity on."""
+        self._is_on = True
+
+    def turn_off(self) -> None:
+        """Turn the entity off."""
+        self._is_on = False
+
+
+class BinarySensorNoDefaultIconIsOnAttr(binary_sensor.BinarySensorEntity):
+    """Test binary sensor with no icon."""
+
+    def __init__(
+        self, binary_sensor_device_class: binary_sensor.BinarySensorDeviceClass
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__()
+        self._attr_is_on = None
+        self.device_class = binary_sensor_device_class
+
+    def turn_on(self) -> None:
+        """Turn the entity on."""
+        self._attr_is_on = True
+
+    def turn_off(self) -> None:
+        """Turn the entity off."""
+        self._attr_is_on = False
+
+
+class BinarySensorWithIcon(BinarySensorNoDefaultIcon):
+    """Test binary sensor with icon."""
+
+    @property
+    def icon(self) -> str:
+        """Return the icon."""
+        return sensor3_icon
+
+
+class BinarySensorWithIconIsOnAttr(BinarySensorNoDefaultIconIsOnAttr):
+    """Test binary sensor with icon."""
+
+    @property
+    def icon(self) -> str:
+        """Return the icon."""
+        return sensor4_icon
+
+
+class BinarySensorWithIconAttr(BinarySensorNoDefaultIcon):
+    """Test binary sensor with icon."""
+
+    def __init__(
+        self, binary_sensor_device_class: binary_sensor.BinarySensorDeviceClass
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__(binary_sensor_device_class)
+        self._attr_icon = sensor5_icon
+
+
+class BinarySensorWithIconAttrIsOnAttr(BinarySensorNoDefaultIconIsOnAttr):
+    """Test binary sensor with icon."""
+
+    def __init__(
+        self, binary_sensor_device_class: binary_sensor.BinarySensorDeviceClass
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__(binary_sensor_device_class)
+        self._attr_icon = sensor6_icon
+
+
+@pytest.mark.parametrize(
+    "device_class", list(binary_sensor.BinarySensorDeviceClass) + [None]
+)
 async def test_default_icon(
     device_class: binary_sensor.BinarySensorDeviceClass, hass: HomeAssistant
 ) -> None:
@@ -155,10 +248,12 @@ async def test_default_icon(
         ),
     )
 
-    entity = binary_sensor.BinarySensorEntity()
-    entity_id_str = "binary_sensor.test"
-    entity.entity_id = entity_id_str
-    entity.device_class = device_class
+    entity1 = BinarySensorNoDefaultIcon(device_class)
+    entity2 = BinarySensorNoDefaultIconIsOnAttr(device_class)
+    entity3 = BinarySensorWithIcon(device_class)
+    entity4 = BinarySensorWithIconIsOnAttr(device_class)
+    entity5 = BinarySensorWithIconAttr(device_class)
+    entity6 = BinarySensorWithIconAttrIsOnAttr(device_class)
 
     async def async_setup_entry_platform(
         hass: HomeAssistant,
@@ -166,7 +261,7 @@ async def test_default_icon(
         async_add_entities: AddEntitiesCallback,
     ) -> None:
         """Set up test binary_sensor platform via config entry."""
-        async_add_entities([entity])
+        async_add_entities([entity1, entity2, entity3, entity4, entity5, entity6])
 
     mock_platform(
         hass,
@@ -178,14 +273,49 @@ async def test_default_icon(
     config_entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
+    assert entity1.icon is None
+    assert entity2.icon is None
+    assert entity3.icon == sensor3_icon
+    assert entity4.icon == sensor4_icon
+    assert entity5.icon == sensor5_icon
+    assert entity6.icon == sensor6_icon
+    # run twice to make sure when state changes it updates icon each time
+    for _ in range(2):
+        entity1.turn_on()
+        entity2.turn_on()
+        entity3.turn_on()
+        entity4.turn_on()
+        entity5.turn_on()
+        entity6.turn_on()
+        if device_class is None:
+            assert entity1.icon is None
+            assert entity2.icon is None
+        else:
+            assert entity1.icon == binary_sensor.DEVICE_ICON_MAP[device_class][0]
+            assert entity2.icon == binary_sensor.DEVICE_ICON_MAP[device_class][0]
+        assert entity3.icon == sensor3_icon
+        assert entity4.icon == sensor4_icon
+        assert entity5.icon == sensor5_icon
+        assert entity6.icon == sensor6_icon
+        entity1.turn_off()
+        entity2.turn_off()
+        entity3.turn_off()
+        if device_class is None:
+            assert entity1.icon is None
+            assert entity2.icon is None
+        else:
+            assert entity1.icon == binary_sensor.DEVICE_ICON_MAP[device_class][1]
+            assert entity2.icon == binary_sensor.DEVICE_ICON_MAP[device_class][1]
+        assert entity3.icon == sensor3_icon
+        assert entity4.icon == sensor4_icon
+        assert entity5.icon == sensor5_icon
+        assert entity6.icon == sensor6_icon
 
-    assert entity.icon is None
-
-    hass.states.async_set(entity_id_str, STATE_ON)
-    assert entity.icon == binary_sensor.DEVICE_ICON_MAP[device_class][0]
-
-    hass.states.async_set(entity_id_str, STATE_OFF)
-    assert entity.icon == binary_sensor.DEVICE_ICON_MAP[device_class][1]
+        # reset entities with no default icon to is_on = None
+        entity1._is_on = None
+        entity2._attr_is_on = None
+        assert entity1.icon is None
+        assert entity2.icon is None
 
 
 async def test_entity_category_config_raises_error(
